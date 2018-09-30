ab0b8aef63d13e306b3a837696626d67a7c17fac
Andrey <soul.evans.15@gmail.com>
2018-10-01 01:39:32 +0300

    completed home section

--- js/index.js
+++ js/index.js
@@ -1,2 +1,4 @@
 'use strict';
 import '../css/style.css';
+import '../bootstrap-grid/bootstrap-grid.min.css';
+import './script';
---
+++ js/script.js
@@ -0,0 +1,8 @@
+// jQuery
+$(function () {
+    let screenHeight = $(window).height();
+    $('.preloader').fadeOut(100);
+    $('window').scroll(function (event) {
+
+    })
+});
