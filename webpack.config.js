fc9bee595e49e0012835a50411b8c6a69f4a01ea
Andrey <soul.evans.15@gmail.com>
2018-09-30 13:04:55 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+    entry: './js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build')
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true // set to true if you want JS source maps
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    devtool: "source-map ",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: [
+                    path.resolve(__dirname),
+                ],
+                loader: 'babel-loader',
+                query: {
+                    plugins: ['transform-class-properties'],
+                    presets: ['@babel/preset-env'],
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader",
+                    options: {
+                        minimize: true
+                    }
+                }
+            },
+            {
+                test: /\.php$/,
+                loaders: [
+                    'html-minify',
+                    'php-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'img/[name].[ext]'
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        })
+    ]
+};
