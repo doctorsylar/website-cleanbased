a5a60cbdd18d0be8e2bd87edc452cb15c8762975
Andrey <soul.evans.15@gmail.com>
2018-10-03 02:24:02 +0300

    almost done with header

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
 'use strict';
 import '../css/style.css';
-import '../bootstrap-grid/bootstrap-grid.min.css';
 import './script';
--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
 // jQuery
 $(function () {
-    let screenHeight = $(window).height();
-    $('.preloader').fadeOut(100);
-    $('window').scroll(function (event) {
+    let windowVar = $(window);
+    let screenHeight = windowVar.height();
 
-    })
+    $('.preloader').fadeOut(100);
+    // anchor links slow scroll
+    $('.anchor').click(function (event) {
+        event.preventDefault();
+        let id = $(this).attr('href');
+        let top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 600);
+    });
+    // scroll events:
+    // 1. Home background smooth scrolling
+    windowVar.scroll(function (event) {
+        let jsWindow = windowVar[0];
+        let windowHeight = jsWindow.innerHeight;
+        let scrollPos = jsWindow.scrollY;
+        if (scrollPos < windowHeight) {
+            $('#home').css('background-position-y', scrollPos / -5);
+        }
+    });
+    // Menu toggler
+    $('.menu-toggler > i').click(function () {
+        $('.vertical-menu').slideToggle();
+    });
 });
